aa4c6ee52490c368d2daa6f5f960d9e65aa21042
vaibhav05cse <vaibhav05cse@gmail.com>
2025-08-16 13:03:09 +0530

    Updated first.py to use bubble sort

--- first.py
+++ first.py
@@ -1,4 +1,4 @@
-# Take n numbers from user and sort them in ascending order
+# Take n numbers from user and sort them in ascending order using Bubble Sort
 
 n = int(input("Enter how many numbers you want to input: "))
 numbers = []
@@ -7,5 +7,11 @@ for i in range(n):
     num = float(input(f"Enter number {i+1}: "))
     numbers.append(num)
 
-numbers.sort()
+# Bubble Sort implementation
+for i in range(len(numbers) - 1):
+    for j in range(len(numbers) - i - 1):
+        if numbers[j] > numbers[j + 1]:
+            # Swap
+            numbers[j], numbers[j + 1] = numbers[j + 1], numbers[j]
+
 print("Sorted numbers in ascending order:", numbers)
